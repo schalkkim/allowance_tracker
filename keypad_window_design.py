5c33449b89bdc43a84c12313ec3d1b1973d87c60
schalkkim <schalk.kim@awatapu.school.nz>
2022-08-22 10:08:25 +1200

    Version 2 of keypad_window_design.py. Made buttons in a loop to reduce amount of code.

--- keypad_window_design.py
+++ keypad_window_design.py
@@ -1,6 +1,6 @@
 # Kim Schalk
 # 17/08/2022
-# Keypad Window Design - Version 1
+# Keypad Window Design - Version 2
 
 # Import Libraries
 from tkinter import *
@@ -32,36 +32,28 @@ password_label = Label(label_border, text="* * * *", fg="black", bg=keypad_butto
                        bd=0)
 password_label.grid(row=0, column=0, ipadx=72, ipady=3, pady=1, padx=1)
 
-# Buttons 1-9
+# Buttons 1-9 in a loop
 button_frame = Frame(root, bg=background_colour)
 button_frame.grid(row=2, column=0, columnspan=3, padx=15, pady=10)
-one_button = Button(button_frame, text="1", fg="black", bg=keypad_buttons_colour, font=("Comfortaa", 12), bd=1,
-                    relief="solid")
-one_button.grid(row=0, column=0, ipadx=19, padx=7, pady=7)
-two_button = Button(button_frame, text="2", fg="black", bg=keypad_buttons_colour, font=("Comfortaa", 12), bd=1,
-                    relief="solid")
-two_button.grid(row=0, column=1, ipadx=17, padx=7, pady=7)
-three_button = Button(button_frame, text="3", fg="black", bg=keypad_buttons_colour, font=("Comfortaa", 12), bd=1,
-                      relief="solid")
-three_button.grid(row=0, column=2, ipadx=17, padx=7, pady=7)
-four_button = Button(button_frame, text="4", fg="black", bg=keypad_buttons_colour, font=("Comfortaa", 12), bd=1,
-                     relief="solid")
-four_button.grid(row=1, column=0, ipadx=17, padx=7, pady=7)
-five_button = Button(button_frame, text="5", fg="black", bg=keypad_buttons_colour, font=("Comfortaa", 12), bd=1,
-                     relief="solid")
-five_button.grid(row=1, column=1, ipadx=17, padx=7, pady=7)
-six_button = Button(button_frame, text="6", fg="black", bg=keypad_buttons_colour, font=("Comfortaa", 12), bd=1,
-                    relief="solid")
-six_button.grid(row=1, column=2, ipadx=17, padx=7, pady=7)
-seven_button = Button(button_frame, text="7", fg="black", bg=keypad_buttons_colour, font=("Comfortaa", 12), bd=1,
-                      relief="solid")
-seven_button.grid(row=2, column=0, ipadx=17, padx=7, pady=7)
-eight_button = Button(button_frame, text="8", fg="black", bg=keypad_buttons_colour, font=("Comfortaa", 12), bd=1,
-                      relief="solid")
-eight_button.grid(row=2, column=1, ipadx=17, padx=7, pady=7)
-nine_button = Button(button_frame, text="9", fg="black", bg=keypad_buttons_colour, font=("Comfortaa", 12), bd=1,
-                     relief="solid")
-nine_button.grid(row=2, column=2, ipadx=17, padx=7, pady=7)
+row = 0
+column = 0
+for button in range(1, 10):
+    numbered_button = Button(button_frame, text=button, fg="black", bg=keypad_buttons_colour, font=("Comfortaa", 12),
+                             bd=1, relief="solid")
+    if button == 4:
+        row += 1
+    elif button == 7:
+        row += 1
+
+    if button == 1:
+        ipadx = 19
+    else:
+        ipadx = 17
+
+    numbered_button.grid(row=row, column=column, ipadx=ipadx, padx=7, pady=7)
+    column += 1
+    if column == 3:
+        column = 0
 
 # Other Buttons (Delete, 0, and Enter)
 delete_button = Button(button_frame, text="Delete", fg="black", bg=keypad_buttons_colour, font=("Comfortaa", 8), bd=1,
