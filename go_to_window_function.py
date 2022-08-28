a7193f40b1fdf0c6693eb13f3d11ccad465b5c58
schalkkim <schalk.kim@awatapu.school.nz>
2022-08-29 09:19:08 +1200

    Version 1 of go_to_window_function.py. Help button opens a new window and is disabled while the window is open. Help button is enabled when the top window is closed.

--- go_to_window_function.py
+++ go_to_window_function.py
@@ -2,3 +2,37 @@
 # 26/08/2022
 # Go to Window Function - Version 1
 
+# Import Libraries
+from tkinter import *
+from matplotlib import font_manager
+from PIL import Image, ImageTk
+
+# Access downloaded fonts on computer
+font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
+
+# Colours used in the program
+background_colour = "#93c47d"
+button_colour = "#d9ead3"
+
+root = Tk()
+root.title("Allowance Tracker")
+root.configure(bg=background_colour)
+
+# Open a New Window
+def go_to_window(button):
+    new_window = Toplevel()
+    new_window.config(bg=background_colour)
+    def close_window(button):
+        button.config(state=NORMAL)
+        new_window.destroy()
+    button.config(state=DISABLED)
+    new_window.protocol("WM_DELETE_WINDOW", lambda: close_window(button))
+
+# Help Button
+help_image = PhotoImage(file="help_image.png")
+help_button = Button(root, image=help_image, fg="black", bg=background_colour, activebackground=background_colour,
+                     font=("Comfortaa", 12), bd=0, relief="solid", command=lambda: go_to_window(help_button))
+help_button.grid(row=0, column=0, pady=7, padx=7, sticky=NE)
+
+# End of Program
+root.mainloop()
