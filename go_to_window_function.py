e0d2b1402afae2b49f2a4abc591114b074bfc0d9
schalkkim <schalk.kim@awatapu.school.nz>
2022-08-30 10:35:08 +1200

    Version 3 of go_to_window_function.py. When help button is pressed, new window is opened and main window is closed. When new window is closed, main window opens again.

--- go_to_window_function.py
+++ go_to_window_function.py
@@ -1,11 +1,10 @@
 # Kim Schalk
 # 26/08/2022
-# Go to Window Function - Version 2
+# Go to Window Function - Version 3
 
 # Import Libraries
 from tkinter import *
 from matplotlib import font_manager
-from PIL import Image, ImageTk
 
 # Access downloaded fonts on computer
 font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
@@ -14,25 +13,42 @@ font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
 background_colour = "#93c47d"
 button_colour = "#d9ead3"
 
-root = Tk()
-root.title("Allowance Tracker")
-root.configure(bg=background_colour)
 
-# Open a New Window
-def go_to_window(button):
-    new_window = Tk()
-    new_window.title("Allowance Tracker")
-    new_window.config(bg=background_colour)
-    root.destroy()
-    def close_window(button):
-        new_window.destroy()
-    new_window.protocol("WM_DELETE_WINDOW", lambda: close_window(button))
+class MainWindow:
+    def __init__(self):
+        self.main_window = Tk()
+        self.main_window.config(bg=background_colour)
+        self.main_window.title("Allowance Tracker")
+        self.main_window.grid()
+        # Help Button
+        self.help_image = PhotoImage(file="help_image.png")
+        self.help_button = Button(self.main_window, image=self.help_image, fg="black", bg=background_colour,
+                                  activebackground=background_colour, font=("Comfortaa", 12), bd=0, relief="solid",
+                                  command=self.go_to_new_window)
+        self.help_button.grid(row=0, column=0, pady=7, padx=7, sticky=NE)
+        self.main_window.mainloop()
 
-# Help Button
-help_image = PhotoImage(file="help_image.png")
-help_button = Button(root, image=help_image, fg="black", bg=background_colour, activebackground=background_colour,
-                     font=("Comfortaa", 12), bd=0, relief="solid", command=lambda: go_to_window(help_button))
-help_button.grid(row=0, column=0, pady=7, padx=7, sticky=NE)
+    def go_to_new_window(self):
+        NewWindow(self)
 
-# End of Program
-root.mainloop()
+
+class NewWindow:
+    def __init__(self, partner):
+        self.new_window = Tk()
+        self.new_window.config(bg=background_colour)
+        self.new_window.title("Allowance Tracker")
+        self.new_window.grid()
+
+        partner.main_window.destroy()
+
+        self.new_window.protocol("WM_DELETE_WINDOW", self.close_window)
+        self.new_window.mainloop()
+
+    def close_window(self):
+        self.new_window.destroy()
+        MainWindow()
+
+
+# main routine
+if __name__ == "__main__":
+    MainWindow()
