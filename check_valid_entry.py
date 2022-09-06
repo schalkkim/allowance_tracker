635c7ebbdf373e68c918abaf78b472dc7bfb5e60
schalkkim <schalk.kim@awatapu.school.nz>
2022-09-07 10:37:03 +1200

    Version 1 of check_valid_entry.py. Check if value is a float.

--- check_valid_entry.py
+++ check_valid_entry.py
@@ -1,3 +1,40 @@
 # Kim Schalk
 # 07/09/2022
 # Check Valid Entry - Version 1
+
+# Import Libraries
+from tkinter import *
+from matplotlib import font_manager
+from tkinter import messagebox
+
+# Access downloaded fonts on computer
+font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
+
+# Colours used in the program
+background_colour = "#93c47d"
+button_colour = "#d9ead3"
+
+class MainWindow:
+    def __init__(self):
+        self.main_window = Tk()
+        self.main_window.title("Accounts Window")
+        self.main_window.config(bg=background_colour)
+
+        self.item_entry = Entry(self.main_window)
+        self.item_entry.grid(row=0, column=0, padx=10, pady=10)
+        self.enter_button = Button(self.main_window, text="Enter", bg=button_colour, font=("Comfortaa", 12),
+                                   bd=1, relief="solid", command=lambda: self.check_valid_entry(self))
+        self.enter_button.grid(row=2, column=0, padx=10, pady=10)
+
+        self.main_window.mainloop()
+
+    def check_valid_entry(self, partner):
+        try:
+            amount = float(partner.item_entry.get())
+        except ValueError:
+            messagebox.showerror("Error", "Not a valid entry")
+
+# main routine
+if __name__ == "__main__":
+    MainWindow()
+
