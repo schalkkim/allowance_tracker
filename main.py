6f565b0f9163c0b2e639cb336037f441ecd1dd92
schalkkim <schalk.kim@awatapu.school.nz>
2022-09-14 09:15:58 +1200

    Version 2 of main.py, help window opens when help button is pressed.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Kim Schalk
 # 09/06/2022
-# Main Program - Version 1 - Keypad
+# Main Program - Version 2 - Help
 
 # Import Libraries
 from tkinter import *
@@ -31,7 +31,7 @@ class Keypad:
         self.help_image = PhotoImage(file="help_image.png")
         self.help_button = Button(self.keypad_window, image=self.help_image, fg="black", bg=background_colour,
                                   activebackground=background_colour,
-                                  font=("Comfortaa", 12), bd=0, relief="solid")
+                                  font=("Comfortaa", 12), bd=0, relief="solid", command=lambda: self.go_to_help(self))
         self.help_button.grid(row=0, column=2, pady=7, padx=7, sticky=E)
 
         # Password Label
@@ -39,7 +39,8 @@ class Keypad:
         self.label_border.grid(row=1, column=0, columnspan=3, pady=10)
         self.asterixes = StringVar()
         self.asterixes.set("")
-        self.password_label = Label(self.label_border, textvariable=self.asterixes, fg="black", bg=keypad_buttons_colour,
+        self.password_label = Label(self.label_border, textvariable=self.asterixes, fg="black",
+                                    bg=keypad_buttons_colour,
                                     font=("Inconsolata", 12),
                                     bd=0)
         self.password_label.grid(row=0, column=0, ipadx=5, ipady=3, pady=1, padx=1)
@@ -109,7 +110,8 @@ class Keypad:
         print(pin_number)
         partner.asterixes.set(partner.asterixes.get() + "* ")
 
-    def delete(self, partner):
+    @staticmethod
+    def delete(partner):
         global pin_number
         partner.asterixes.set("")
         pin_number = ""
@@ -126,6 +128,77 @@ class Keypad:
             print(self.access)
         else:
             self.delete(partner)
+
+    @staticmethod
+    def go_to_help(partner):
+        HelpWindow(partner)
+
+
+class HelpWindow:
+    def __init__(self, partner):
+        self.help_window = Toplevel()
+        self.help_window.config(bg=background_colour)
+
+        partner.help_button.config(state=DISABLED)
+        self.help_window.protocol("WM_DELETE_WINDOW", lambda: self.close_window(self, partner))
+
+        # Components
+        self.name_label = Label(self.help_window, text="Help", font=("Comfortaa", 25, "bold"), bg=background_colour)
+        self.name_label.grid(row=0, column=0, padx=5, pady=(15, 10))
+
+        self.transactions_frame = Frame(self.help_window)
+        self.transactions_frame.grid(row=1, column=0, padx=20, pady=(0, 30), stick="nw")
+        self.transactions_frame.grid_rowconfigure(0, weight=1)
+        self.transactions_frame.grid_columnconfigure(0, weight=1)
+        self.transactions_frame.grid_propagate(False)
+        self.transactions_canvas = Canvas(self.transactions_frame, bg=button_colour)
+        self.transactions_canvas.grid(row=0, column=0, sticky=NSEW)
+        self.transactions_scrollbar = Scrollbar(self.transactions_frame, orient="vertical",
+                                                command=self.transactions_canvas.yview)
+        self.transactions_scrollbar.grid(row=0, column=1, sticky=NS)
+        self.transactions_canvas.configure(yscrollcommand=self.transactions_scrollbar.set)
+        self.transactions_labels_frame = Frame(self.transactions_canvas)
+        self.transactions_canvas.create_window((0, 0), window=self.transactions_labels_frame, anchor="nw")
+
+        information = ("To start enter your pin\n"
+                       "\n"
+                       "Choose the child who's account you \n"
+                       "would like to view, if you only have\n"
+                       "one child, you will go straight to\n"
+                       "the accounts page.\n"
+                       "\n"
+                       "On the accounts page you can view\n"
+                       "the amount of money the child has\n"
+                       "and their previous transactions. You\n"
+                       "can sort their transactions by month\n"
+                       "by using the dropdown menu to choose\n"
+                       "a month, or by using the arrows to\n"
+                       "change months.\n"
+                       "To add money to a child, push the\n"
+                       "\"add payment\" button. This will take\n"
+                       "you to a window where you can enter\n"
+                       "the reason and the amount.\n"
+                       "To remove money, push the \"add\n"
+                       "transaction\" button. This will take\n"
+                       "you to a window where you can add the\n"
+                       "item they bought and the cost.\n"
+                       "To export their previous transactions,\n"
+                       "press the \"export\" button.")
+
+        self.information_label = Label(self.transactions_labels_frame, text=information, bg=button_colour,
+                                       font=("Comfortaa", 9), justify=LEFT)
+        self.information_label.grid(row=0, column=0)
+
+        # Allows resizing of labels
+        self.transactions_labels_frame.update_idletasks()
+        # Change size of the box
+        self.transactions_frame.config(width=260, height=260)
+        # Set scrolling region
+        self.transactions_canvas.config(scrollregion=self.transactions_canvas.bbox("all"))
+
+    def close_window(self, partner, partner2):
+        partner.help_window.destroy()
+        partner2.help_button.config(state=ACTIVE)
 
 
 # main routine
