f1056c1becdce4104c84a0382d2606fbaa61ea19
schalkkim <schalk.kim@awatapu.school.nz>
2022-09-21 11:36:31 +1200

    Version 6 of main.py, transaction window added. Amount is removed from label and transaction is added to scrollbar.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Kim Schalk
 # 09/06/2022
-# Main Program - Version 5 - Parent Access (Account)
+# Main Program - Version 6 - Transaction
 
 # Import Libraries
 from tkinter import *
@@ -20,16 +20,21 @@ parent_access_pin = "1234"
 pin_number = ""
 
 # Child Information
-child_information = [["Nikau", "nikau_profile_icon.png", 102.00, "0000"],
-                     ["Hana", "hana_profile_icon.png", 98.01, "4567"],
-                     ["Tia", "tia_profile_icon.png", 56.00, "8765"]]
+child_information = [["Nikau", "nikau_profile_icon.png", "102.00", "0000"],
+                     ["Hana", "hana_profile_icon.png", "98.01", "4567"],
+                     ["Tia", "tia_profile_icon.png", "56.00", "8765"]]
+
+# Transaction Lists
+for child in child_information:
+    transaction = []
+    child.append(transaction)
 
 # Images
 for child in child_information:
     child.append(Image.open(child[1]))
 
 for child in child_information:
-    child[4] = child[4].resize((45, 45))
+    child[5] = child[5].resize((45, 45))
 
 
 # Keypad window
@@ -43,7 +48,7 @@ class Keypad:
         help_image = PhotoImage(file="help_image.png")
         self.help_button = Button(self.keypad_window, image=help_image, fg="black", bg=background_colour,
                                   activebackground=background_colour,
-                                  font=("Comfortaa", 12), bd=0, relief="solid", command=lambda: self.go_to_help(self))
+                                  font=("Comfortaa", 12), bd=0, relief="solid", command=lambda: self.go_to_help())
         self.help_button.grid(row=0, column=2, pady=7, padx=7, sticky=E)
 
         # Password Label
@@ -66,7 +71,7 @@ class Keypad:
         for button in range(1, 10):
             self.numbered_button = Button(self.button_frame, text=button, fg="black", bg=keypad_buttons_colour,
                                           font=("Comfortaa", 12), bd=1, relief="solid",
-                                          command=lambda number=button: self.pin_number(self, number))
+                                          command=lambda number=button: self.pin_number(number))
             if button == 4:
                 row += 1
             elif button == 7:
@@ -84,53 +89,46 @@ class Keypad:
 
         # Other Buttons (Delete, 0, and Enter)
         self.delete_button = Button(self.button_frame, text="Delete", fg="black", bg=keypad_buttons_colour,
-                                    font=("Comfortaa", 8), bd=1, relief="solid", command=lambda: self.delete(self))
+                                    font=("Comfortaa", 8), bd=1, relief="solid", command=lambda: self.delete())
         self.delete_button.grid(row=3, column=0, ipadx=7, ipady=7, padx=7, pady=7)
         self.zero_button = Button(self.button_frame, text="0", fg="black", bg=keypad_buttons_colour,
                                   font=("Comfortaa", 12), bd=1, relief="solid",
-                                  command=lambda: self.pin_number(self, "0"))
+                                  command=lambda: self.pin_number("0"))
         self.zero_button.grid(row=3, column=1, ipadx=17, padx=7, pady=7)
         self.enter_button = Button(self.button_frame, text="Enter", fg="black", bg=keypad_buttons_colour,
-                                   font=("Comfortaa", 8), bd=1, relief="solid", command=lambda: self.enter(self))
+                                   font=("Comfortaa", 8), bd=1, relief="solid", command=lambda: self.enter())
         self.enter_button.grid(row=3, column=2, ipadx=9, ipady=7, padx=7, pady=7)
 
         # End of Program
         self.keypad_window.mainloop()
 
     # Get pin number
-    @staticmethod
-    def pin_number(partner, number):
+    def pin_number(self, number):
         global pin_number
         pin_number += str(number)
-        partner.asterixes.set(partner.asterixes.get() + "* ")
+        self.asterixes.set(self.asterixes.get() + "* ")
 
-    @staticmethod
-    def delete(partner):
+    def delete(self):
         global pin_number
-        partner.asterixes.set("")
+        self.asterixes.set("")
         pin_number = ""
 
-    def enter(self, partner):
+    def enter(self):
         global parent_access_pin
         global child_information
         global pin_number
         for child in child_information:
             if pin_number in child:
-                self.access = False
-                self.child = child
-                print(self.child)
-                partner.keypad_window.destroy()
-                Account(self)
+                self.keypad_window.destroy()
+                Account(False, child)
         if pin_number == parent_access_pin:
-            self.access = True
-            partner.keypad_window.destroy()
-            ChildSelection(self)
+            self.keypad_window.destroy()
+            ChildSelection()
         else:
-            self.delete(partner)
+            self.delete()
 
-    @staticmethod
-    def go_to_help(partner):
-        HelpWindow(partner)
+    def go_to_help(self):
+        HelpWindow(self)
 
 
 class HelpWindow:
@@ -139,7 +137,7 @@ class HelpWindow:
         self.help_window.config(bg=background_colour)
 
         partner.help_button.config(state=DISABLED)
-        self.help_window.protocol("WM_DELETE_WINDOW", lambda: self.close_window(self, partner))
+        self.help_window.protocol("WM_DELETE_WINDOW", lambda: self.close_window(partner))
 
         # Components
         self.name_label = Label(self.help_window, text="Help", font=("Comfortaa", 25, "bold"), bg=background_colour)
@@ -195,27 +193,26 @@ class HelpWindow:
         # Set scrolling region
         self.information_canvas.config(scrollregion=self.information_canvas.bbox("all"))
 
-    @staticmethod
-    def close_window(partner, partner2):
-        partner.help_window.destroy()
-        partner2.help_button.config(state=ACTIVE)
+    def close_window(self, partner):
+        self.help_window.destroy()
+        partner.help_button.config(state=ACTIVE)
 
 
 class Account:
-    def __init__(self, partner):
+    def __init__(self, access, child):
         self.account_window = Tk()
         self.account_window.title("Allowance Tracker")
         self.account_window.configure(bg=background_colour)
 
-        if partner.access:
-            self.account_window.protocol("WM_DELETE_WINDOW", lambda: self.close_window(self, partner))
+        if access:
+            self.account_window.protocol("WM_DELETE_WINDOW", lambda: self.close_window())
 
         # Profile Image
-        partner.child.append(ImageTk.PhotoImage(partner.child[4]))
-        self.image_panel = Label(self.account_window, image=partner.child[-1], bg=background_colour)
+        child.append(ImageTk.PhotoImage(child[5]))
+        self.image_panel = Label(self.account_window, image=child[-1], bg=background_colour)
         self.image_panel.grid(row=0, column=0, padx=5, pady=(20, 0))
 
-        self.name_label = Label(self.account_window, text=partner.child[0], font=("Comfortaa", 25, "bold"),
+        self.name_label = Label(self.account_window, text=child[0], font=("Comfortaa", 25, "bold"),
                                 bg=background_colour)
         self.name_label.grid(row=0, column=1, padx=5, pady=(20, 0), sticky="w")
 
@@ -223,23 +220,28 @@ class Account:
         self.help_image = PhotoImage(file="help_image.png")
         self.help_button = Button(self.account_window, image=self.help_image, fg="black", bg=background_colour,
                                   activebackground=background_colour, font=("Comfortaa", 12), bd=0, relief="solid",
-                                  command=lambda: self.go_to_help(self))
+                                  command=lambda: self.go_to_help())
         self.help_button.grid(row=0, column=2, pady=7, padx=7, sticky=NE)
+
+        # Setting money amount as a variable
+        self.money = StringVar()
+        self.money.set("${:.2f}".format(float(child[2])))
+        child.append(self.money)
 
         # Account Labels
         self.account_balance_label = Label(self.account_window, text="Account Balance:", bg=background_colour,
                                            font=("Comfortaa", 10))
         self.account_balance_label.grid(row=1, column=0, columnspan=3, pady=(5, 0))
-        self.amount_label = Label(self.account_window, text="${:.2f}".format(partner.child[2]), bg=background_colour,
+        self.amount_label = Label(self.account_window, textvariable=child[-1], bg=background_colour,
                                   font=("Comfortaa", 22))
         self.amount_label.grid(row=2, column=0, columnspan=3, pady=(0, 10))
         self.bonus_frame = Frame(self.account_window, bg=button_colour)
         self.bonus_frame.grid(row=3, column=0, columnspan=3, pady=(0, 10))
 
-        if partner.child[2] > 50:
-            bonus = "{} is on track for a bonus!".format(partner.child[0])
+        if float(child[2]) > 50:
+            bonus = "{} is on track for a bonus!".format(child[0])
         else:
-            bonus = "{} needs to earn money to get the bonus!".format(partner.child[0])
+            bonus = "{} needs to earn money to get the bonus!".format(child[0])
 
         self.bonus_label1 = Label(self.bonus_frame, text=bonus, bg=button_colour, font=("Comfortaa", 9))
         self.bonus_label1.grid(row=0, column=0, padx=20, pady=(5, 0))
@@ -277,19 +279,23 @@ class Account:
         self.transactions_canvas.configure(yscrollcommand=self.transactions_scrollbar.set)
         self.transactions_labels_frame = Frame(self.transactions_canvas)
         self.transactions_canvas.create_window((0, 0), window=self.transactions_labels_frame, anchor="nw")
+
         row = 0
-        for transaction in range(1, 6):
-            if transaction % 2 == 0:
+        transactions = 0
+        for transaction in child[4]:
+            transactions += 1
+            if transactions % 2 == 0:
                 colour = "#d3d3d3"
             else:
                 colour = "#f3f3f3"
             self.transaction_background = Label(self.transactions_labels_frame, bd=0, bg=colour)
             self.transaction_background.grid(row=row, column=0, columnspan=2, ipady=3)
             self.transaction_background.config(width=28)
-            self.transaction_label = Label(self.transactions_labels_frame, text="Tennis Shoes", font=("Comfortaa", 10),
+            self.transaction_label = Label(self.transactions_labels_frame, text=transaction[0], font=("Comfortaa", 10),
                                            bd=0, bg=colour)
             self.transaction_label.grid(row=row, column=0, sticky="w")
-            self.transaction_cost_label = Label(self.transactions_labels_frame, text="-$123.00 ",
+            self.transaction_cost_label = Label(self.transactions_labels_frame,
+                                                text="${:.2f} ".format(float(transaction[1])),
                                                 font=("Comfortaa", 10), bd=0, bg=colour)
             self.transaction_cost_label.grid(row=row, column=1, sticky="e")
             row += 1
@@ -305,11 +311,12 @@ class Account:
         self.button_frame = Frame(self.account_window, bg="black")
         self.button_frame.grid(row=7, column=0, columnspan=3, pady=(0, 20))
         self.add_transaction_button = Button(self.button_frame, text="Add Transaction", bg=button_colour,
-                                             font=("Comfortaa", 9), bd=0, relief="solid")
+                                             font=("Comfortaa", 9), bd=0, relief="solid",
+                                             command=lambda: self.transaction(access, child))
         self.add_transaction_button.grid(row=0, column=0, padx=(1, 1), pady=(1, 1))
         self.add_transaction_button.config(width="13")
 
-        if partner.access:
+        if access:
             self.add_payment_button = Button(self.button_frame, text="Add Payment", bg=button_colour,
                                              font=("Comfortaa", 9), bd=0, relief="solid")
             self.add_payment_button.grid(row=0, column=1, padx=(0, 1), pady=(1, 1))
@@ -331,44 +338,48 @@ class Account:
         # End of Program
         self.account_window.mainloop()
 
-    @staticmethod
-    def go_to_help(partner):
-        HelpWindow(partner)
+    def go_to_help(self):
+        HelpWindow(self)
 
-    def close_window(self, partner, partner2):
-        partner.account_window.destroy()
-        self.access = partner2.access
-        ChildSelection(self)
+    def transaction(self, access, child):
+        Transaction(self, access, child)
+
+    def close_window(self):
+        self.account_window.destroy()
+        ChildSelection()
 
 
 class ChildSelection:
-    def __init__(self, partner):
+    def __init__(self):
         self.child_selection_window = Tk()
         self.child_selection_window.title("Allowance Tracker")
         self.child_selection_window.config(bg=background_colour)
-
-        self.access = partner.access
 
         # Help Button
         self.help_image = PhotoImage(file="help_image.png")
         self.help_button = Button(self.child_selection_window, image=self.help_image, fg="black", bg=background_colour,
                                   activebackground=background_colour, font=("Comfortaa", 12), bd=0, relief="solid",
-                                  command=lambda: self.go_to_help(self))
+                                  command=lambda: self.go_to_help())
         self.help_button.grid(row=0, column=0, pady=7, padx=7, sticky=E)
 
         # Child buttons in a loop
         row = 1
         for child in child_information:
             # Resizing Images
-            child.append(ImageTk.PhotoImage(child[4]))
+            child.append(ImageTk.PhotoImage(child[5]))
+
+            # Setting money amount as a variable
+            self.money = StringVar()
+            self.money.set("${:.2f}".format(float(child[2])))
+            child.append(self.money)
 
             # Buttons
             self.button_frame = Frame(self.child_selection_window, bd=1, relief="solid", bg=button_colour)
             self.button_frame.grid(row=row, column=0, ipadx=3, padx=10, pady=5)
-            text = "  {} - ${:.2f}".format(child[0].capitalize(), child[2])
-            self.name_button = Button(self.button_frame, text=text, image=child[-1], compound=LEFT, fg="black",
+            text = "  {} - {}".format(child[0].capitalize(), child[-1].get())
+            self.name_button = Button(self.button_frame, text=text, image=child[-2], compound=LEFT, fg="black",
                                       bg=button_colour, font=("Comfortaa", 12), bd=0, relief="solid", anchor="w",
-                                      command=lambda name=child[0]: self.go_to_account(self, name))
+                                      command=lambda name=child[0]: self.go_to_account(name))
             self.name_button.grid(row=0, column=0, pady=5, padx=10, ipady=4)
             self.name_button.config(width=185)
             if child == child_information[-1]:
@@ -378,20 +389,65 @@ class ChildSelection:
         # End of Program
         self.child_selection_window.mainloop()
 
-    @staticmethod
-    def go_to_help(partner):
-        HelpWindow(partner)
+    def go_to_help(self):
+        HelpWindow(self)
 
-    def go_to_account(self, partner, name):
-        self.access = True
+    def go_to_account(self, name):
         for child in child_information:
             if name in child:
-                self.child = child
-        partner.child_selection_window.destroy()
-        Account(self)
+                person = child
+        self.child_selection_window.destroy()
+        Account(True, person)
+
+
+class Transaction:
+    def __init__(self, partner, access, child):
+        self.transaction_window = Toplevel()
+        self.transaction_window.config(bg=background_colour)
+
+        child.append(ImageTk.PhotoImage(child[5]))
+        self.image_panel = Label(self.transaction_window, image=child[-1], bg=background_colour)
+        self.image_panel.grid(row=0, column=0, padx=5, pady=(20, 0))
+
+        self.name_label = Label(self.transaction_window, text="Transaction", font=("Comfortaa", 25, "bold"),
+                                bg=background_colour)
+        self.name_label.grid(row=0, column=1, padx=5, pady=(20, 0), sticky="w")
+
+        # Help Button
+        help_image = PhotoImage(file="help_image.png")
+        self.help_button = Button(self.transaction_window, image=help_image, fg="black", bg=background_colour,
+                                  activebackground=background_colour,
+                                  font=("Comfortaa", 12), bd=0, relief="solid")
+        self.help_button.grid(row=0, column=2, pady=7, padx=7, sticky=NE)
+
+        # Input Labels and Entries
+        self.item_label = Label(self.transaction_window, text="Item:", bg=background_colour,
+                                font=("Comfortaa", 12, "bold"))
+        self.item_label.grid(row=1, column=0, columnspan=2, sticky="w", padx=20, pady=(20, 10))
+        self.item_entry = Entry(self.transaction_window, font=("Comfortaa", 10), bd=1)
+        self.item_entry.grid(row=2, column=0, columnspan=3, padx=15)
+        self.item_entry.config(width=40)
+        self.cost_label = Label(self.transaction_window, text="Cost:", bg=background_colour,
+                                font=("Comfortaa", 12, "bold"))
+        self.cost_label.grid(row=3, column=0, columnspan=2, sticky="w", padx=20, pady=(20, 10))
+        self.cost_entry = Entry(self.transaction_window, font=("Comfortaa", 10), bd=1)
+        self.cost_entry.grid(row=4, column=0, columnspan=3, padx=15)
+        self.cost_entry.config(width=40)
+
+        self.enter_button = Button(self.transaction_window, text="Enter", bg=button_colour, font=("Comfortaa", 12),
+                                   bd=1, relief="solid", command=lambda: self.add_transaction(partner, access, child))
+        self.enter_button.grid(row=5, column=0, columnspan=3, pady=(30, 20), ipadx=25)
+
+    def add_transaction(self, partner, access, child):
+        cost = float(self.cost_entry.get())
+        child[2] = str(float(child[2]) - cost)
+        transaction1 = [self.item_entry.get(), self.cost_entry.get()]
+        child[4].append(transaction1)
+        self.transaction_window.destroy()
+        partner.account_window.destroy()
+        Account(access, child)
 
 
 # main routine
 if __name__ == "__main__":
     Keypad()
-
